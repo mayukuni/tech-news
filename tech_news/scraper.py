f2ce2e72329dd9335146b8076b7cfccbd0c65a2b
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2023-01-25 02:21:22 -0300

    req 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,9 +36,37 @@ def scrape_next_page_link(html_content: str):
     else:
         return None
 
+
 # Requisito 4
-def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+def scrape_news(html_content: str):
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css("h1::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".author a::text").get()
+    comments_count = selector.css(
+        ".post-comments .title-block::text"
+    ).get()
+    summary = "".join(selector.css(
+        ".entry-content > p:first-of-type *::text"
+    ).getall()).strip()
+    tags = selector.css(".post-tags ul li a::text").getall()
+    category = selector.css(".meta-category a .label::text").get()
+
+    data = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": (
+            int(comments_count.split()[0])
+            if comments_count else 0
+        ),
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
+    return data
 
 
 # Requisito 5
